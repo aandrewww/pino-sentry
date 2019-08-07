1a13a39db3f223d56e8171438d53b826680208a9
Andrew Avdeev <andrewww.avdeev@gmail.com>
2019-08-07 15:32:07 +0300

    init

---
+++ README.md
@@ -0,0 +1,61 @@
+
+# pino-sentry
+[![CircleCI](https://circleci.com/gh/aandrewww/pino-sentry.svg?style=svg)](https://circleci.com/gh/aandrewww/pino-sentry)
+[![node](https://img.shields.io/badge/node-6.4.0+-brightgreen.svg)][node-url]
+[![license](https://img.shields.io/github/license/aandrewww/pino-sentry.svg)][license-url]
+
+Load [pino](https://github.com/pinojs/pino) logs into [Sentry](https://sentry.io/)
+
+## Index
+
+* [Install](#install)
+* [Usage](#usage)
+* [Options](#options-options)
+  - [Transport options](#transport-options)
+  - [Log Level Mapping](#log-level-mapping)
+* [License](#license)
+
+## Install
+
+```bash
+npm install pino-sentry -g
+```
+
+## Usage
+
+```bash
+node ./app.js | pino-sentry --dsn=https://******@sentry.io/12345
+```
+
+## Options (`options`)
+
+### Transport options
+
+* `--dsn` (`-d`): your Sentry DSN or Data Source Name (defaults to `process.env.SENTRY_DSN`)
+* `--environment` (`-e`): (defaults to `process.env.SENTRY_ENVIRONMENT || process.env.NODE_ENV || 'production'`)
+* `--serverName` (`-n`): transport name (defaults to `pino-sentry`)
+* `--debug` (`-dm`): turns debug mode on or off (default to `process.env.SENTRY_DEBUG || false`)
+* `--sampleRate` (`-sr`): sample rate as a percentage of events to be sent in the range of 0.0 to 1.0 (default to `1.0`)
+* `--maxBreadcrumbs` (`-mx`): total amount of breadcrumbs that should be captured (default to `100`)
+
+### Log Level Mapping
+
+Pino logging levels are mapped by default to Sentry's acceptable levels.
+
+```js
+{
+  trace: 'debug',
+  debug: 'debug',
+  info: 'info',
+  warn: 'warning',
+  error: 'error',
+  fatal: 'fatal'
+}
+```
+
+## License
+
+[MIT License][license-url]
+
+[license-url]: LICENSE
+[node-url]: https://nodejs.org
---
+++ src/transport.ts
@@ -0,0 +1,116 @@
+import stream from 'stream';
+import split from 'split2';
+import pump from 'pump';
+import through from 'through2';
+import pify from 'pify';
+import * as Sentry from '@sentry/node';
+
+class ExtendedError extends Error {
+  public constructor(info: any) {
+    super(info.message);
+
+    this.name = "Error";
+    this.stack = info.stack || null;
+  }
+}
+
+class PinoSentryTransport {
+  private SEVERITIES_MAP = {
+    10: Sentry.Severity.Debug,   // pino: trace
+    20: Sentry.Severity.Debug,   // pino: debug
+    30: Sentry.Severity.Info,    // pino: info
+    40: Sentry.Severity.Warning, // pino: warn
+    50: Sentry.Severity.Error,   // pino: error
+    60: Sentry.Severity.Fatal,   // pino: fatal
+  };
+
+  public constructor (options?: Sentry.NodeOptions) {
+    Sentry.init(this.withDefaults(options || {}));
+  }
+
+  public getLogSeverity(level: number): Sentry.Severity  {
+    return (this.SEVERITIES_MAP as any)[level];
+  }
+
+  public get sentry() {
+    return Sentry;
+  }
+
+  public transformer(): stream.Transform {
+    return through.obj((chunk: any, _enc: any, cb: any) => {
+      const severity = this.getLogSeverity(chunk.level);
+      const tags = chunk.tags || {};
+
+      if (chunk.reqId) {
+        tags.uuid = chunk.reqId;
+      }
+
+      if (chunk.responseTime) {
+        tags.responseTime = chunk.responseTime;
+      }
+
+      if (chunk.hostname) {
+        tags.hostname = chunk.hostname;
+      }
+
+      // const user = chunk.user || {};
+
+      let message = chunk.message;
+      let stack = chunk.stack || '';
+
+      Sentry.configureScope(scope => {
+        if (this.isObject(tags)) {
+          Object.keys(tags).forEach(tag => scope.setExtra(tag, tags[tag]));
+        }
+      });
+
+      // Capturing Errors / Exceptions
+      if (this.shouldLogException(severity)) {
+        const error = message instanceof Error ? message : new ExtendedError({ message, stack });
+
+        setImmediate(() => {
+          Sentry.captureException(error);
+          cb();
+        });
+      } else {
+        // Capturing Messages
+        setImmediate(() => {
+          Sentry.captureMessage(message, severity);
+          cb();
+        });
+      }
+    });
+  }
+
+  private withDefaults(options: Sentry.NodeOptions) {
+    return {
+      dsn: options.dsn || process.env.SENTRY_DSN || '',
+      serverName: options && options.serverName || 'pino-sentry',
+      environment: options && options.environment || process.env.SENTRY_ENVIRONMENT || process.env.NODE_ENV || 'production',
+      debug: options && options.debug || !!process.env.SENTRY_DEBUG || false,
+      sampleRate: options && options.sampleRate || 1.0,
+      maxBreadcrumbs: options && options.maxBreadcrumbs || 100,
+    };
+  }
+
+  private isObject(obj: any) {
+    const type = typeof obj;
+    return type === 'function' || type === 'object' && !!obj;
+  }
+
+  private shouldLogException(level: Sentry.Severity) {
+    return level === Sentry.Severity.Fatal || level === Sentry.Severity.Error;
+  }
+};
+
+export function createWriteStream(options: any = {}) {
+  if (!options.dsn && !process.env.SENTRY_DSN) {
+    throw Error('Sentry DSN missing');
+  };
+
+  const transport = new PinoSentryTransport(options);
+  const sentryTransformer = transport.transformer();
+
+  const pumpAsync = pify(pump);
+  return pumpAsync(process.stdin, split(JSON.parse), sentryTransformer);
+};
