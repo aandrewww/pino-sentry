3c1625349d8ac513232db4d009e1d00d57a6a0e4
Andrew Avdeev <andrewww.avdeev@gmail.com>
2020-05-07 16:32:38 +0300

    remove test dsn

--- test/test.js
+++ test/test.js
@@ -1,19 +1,19 @@
-const pinoLogger = require('../node_modules/pino');
-const { createWriteStream } = require('../dist/index');
+// const pinoLogger = require('../node_modules/pino');
+// const { createWriteStream } = require('../dist/index');
 
-async function main () {
-  const SENTRY_DSN = "https://0d5b2413b1a04f538b11999a61d37d2b@sentry.scorum.com/1";
+// async function main () {
+//   const SENTRY_DSN = "";
 
-  const options = {
-    level: "info"
-  };
+//   const options = {
+//     level: "info"
+//   };
 
-  const stream = createWriteStream({ dsn: SENTRY_DSN });
+//   const stream = createWriteStream({ dsn: SENTRY_DSN });
 
-  const logger = pinoLogger(options, stream);
+//   const logger = pinoLogger(options, stream);
 
-  logger.info('testtt info log');
-  logger.error('testtt log');
-}
+//   logger.info('testtt info log');
+//   logger.error('testtt log');
+// }
 
-main();
+// main();
