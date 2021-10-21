49197fb307bb0abe7b1c7c11dd8c86c6123d7d38
Elan Ruusamäe <glen@delfi.ee>
2021-10-21 09:53:28 +0300

    fixup! Try to use pino-abstract-transport

--- src/PinoSentryTransport.ts
+++ src/PinoSentryTransport.ts
@@ -1,9 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const build = require('pino-abstract-transport');
 
-export default async function (opts) {
-  return build(async function (source) {
-    for await (let obj of source) {
+export default async function () {
+  return build(async function (source: any) {
+    for await (const obj of source) {
       console.log(obj);
     }
   });
